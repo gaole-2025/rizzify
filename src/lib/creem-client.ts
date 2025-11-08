f888d2642943d621b5171d15af966c701ad046c8
gaole-2025 <le13107621169@gmail.com>
2025-11-09 07:04:24 +0800

    fix: use correct test API endpoint https://test-api.creem.io for test mode

--- src/lib/creem-client.ts
+++ src/lib/creem-client.ts
@@ -43,7 +43,7 @@ class CreemClient {
     this.apiKey = apiKey;
     // 根据 API 密钥前缀判断环境
     if (apiKey.startsWith('creem_test_')) {
-      this.baseUrl = 'https://api.sandbox.creem.io/v1';
+      this.baseUrl = 'https://test-api.creem.io/v1';
     } else {
       this.baseUrl = 'https://api.creem.io/v1';
     }
